454acd6ee1a7204b50283742dae8a3a1be1c1368
Nick Bargovic <nbargovic@confluent.io>
2023-05-04 15:04:08 -0400

    adding ksql-migrations example

--- ksql-migrations/project1/migrations/V000002__project1_update_order_events.sql
+++ ksql-migrations/project1/migrations/V000002__project1_update_order_events.sql
@@ -3,19 +3,10 @@ CREATE OR REPLACE STREAM ORDER_EVENTS (
   name STRING,
   items ARRAY<STRING>,
   cost DECIMAL(6, 2),
-  ordertime TIMESTAMP
+  ordertime TIMESTAMP,
+  description STRING
 ) WITH (
   kafka_topic='order_event',
   partitions=1,
   value_format='json'
 );
-
-CREATE OR REPLACE STREAM ORDER_NAMES_PERSISTENT
-WITH(
-  KAFKA_TOPIC = 'order-names-persistent',
-  PARTITIONS = 1
-)
-AS SELECT
-  orderid,
-  name
-FROM ORDER_EVENTS;
